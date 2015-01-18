5f3e5545658f0daddbbf6259c21a3d94d4357a10
RomanK <RomanK@192.168.1.29>
2015-01-18 18:37:08 +0100

    amélioration du code via sonarqcube

--- src/aquam/ExperienceUML.java
+++ src/aquam/ExperienceUML.java
@@ -22,24 +22,21 @@ public class ExperienceUML {
    static org.jdom2.Document documentdoc;
    static Element racinedoc;
 
-    public ExperienceUML() {
+    private ExperienceUML() {
     }
    
    
 
-   public static void operationPrincipale()
-   {
+   public static void operationPrincipale(){
       //On crée une instance de SAXBuilder
       SAXBuilder sxb = new SAXBuilder();
-      try
-      {
+      try{
          //On crée un nouveau document JDOM avec en argument le fichier XML
          //Le parsing est terminé 
          documentdoc = sxb.build(new File("C:\\Users\\RomanK\\Desktop\\easyaquamuml.xml"));
          
-      }
-      catch(Exception e){
-          e.printStackTrace();
+      } catch(Exception e){
+          
       }
       
       //On initialise un nouvel élément racine avec l'élément racine du document.
@@ -51,77 +48,14 @@ public class ExperienceUML {
       insertExperience(racinedoc);
       
    }
-/*   
-static void afficheALL()
-{
-    
-    List listWorkbook = racinedoc.getChildren("Workbook");
-    Iterator m = listWorkbook.iterator();
-    while(m.hasNext())
-    {
-        
-    
-    Element courantWorkbook = (Element)m.next();
-   
-    List listWorksheet = courantWorkbook.getChildren("Worksheet");
 
-    //On crée un Iterator sur notre liste
-    Iterator i = listWorksheet.iterator();
-    while(i.hasNext())
-    {
-      
-       
-       Element courantWorksheet = (Element)i.next();
-       List listTable = courantWorksheet.getChildren("Table");
-
-        //On crée un Iterator sur notre liste
-       Iterator j = listTable.iterator();
-       while(j.hasNext())
-       {
-           Element courantTable = (Element)j.next();
-           List listRow = courantTable.getChildren("Row");
-
-           //On crée un Iterator sur notre liste
-           Iterator k = listRow.iterator();
-           while(k.hasNext())
-           {
-               Element courantRow = (Element)k.next();
-               List listCell = courantRow.getChildren("Cell");
-               
-               Iterator l = listCell.iterator();
-               while(l.hasNext())
-               {
-                   Element courantCell = (Element)l.next();
-                   Element courantData = courantCell.getChild("Data");
-                   System.out.println(courantData.getValue());
-                   System.out.println(courantData.getTextTrim());
-                   switch (courantData.getAttributeValue("Type")) {
-                       case "Number":
-                           System.out.println(courantData.getValue());
-                           break;
-                       case "String":
-                           System.out.println(courantData.getTextTrim());
-                           break;
-                   }
-               }
-                   
-               
-           }
-            
-        }  
-    }
-    }
-}
-*/
-static void affiche()
-{
-   try
-   {
+static void affiche(){
+   try{
       //On utilise ici un affichage classique avec getPrettyFormat()
       XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
       sortie.output(documentdoc, System.out);
+   }catch (java.io.IOException e){
    }
-   catch (java.io.IOException e){}
 }
 
 static void afficheDatas(Element source){
@@ -133,20 +67,17 @@ static void afficheDatas(Element source){
     
     while(i.hasNext()){
         Element testSuivant = (Element)i.next();
-        if (testSuivant.getName().equals("Data")){
+        if ("Data".equals(testSuivant.getName())){
             switch (testSuivant.getAttributeValue("Type")) {
                        case "Number":
                            System.out.println(testSuivant.getValue());
                            break;
                        case "String":
                            String nom = testSuivant.getTextTrim();
-                           nom = nom.replace("%","percent");
-                           nom = nom.replace("(","");
-                           nom = nom.replace(")","");
-                           nom = nom.replace("/","_per_");
-                           nom = nom.replace("°","degre");
-                           nom = nom.replace("²","square");
+                           nom = makeStringSQLReadable(nom);
                            System.out.println(nom);
+                           break;
+                       default:
                            break;
                    }
             
@@ -159,28 +90,6 @@ static void afficheDatas(Element source){
         
     }
     
-    
-    
-    
-    
-    /*try{
-    while(descendants.hasNext()){
-        
-        Element courantDescendant = (Element)descendants.next();
-        
-        if(courantDescendant.getName().equals("Data"))
-        {
-            switch (courantDescendant.getAttributeValue("Type")) {
-                       case "Number":
-                           System.out.println(courantDescendant.getValue());
-                           break;
-                       case "String":
-                           System.out.println(courantDescendant.getTextTrim());
-                           break;
-                   }
-        }
-    }
-    } catch(Exception e){e.printStackTrace();}*/
 }
 
 static Element searchFirstRow(Element source){
@@ -192,7 +101,7 @@ static Element searchFirstRow(Element source){
     
     while(i.hasNext()){
         Element testSuivant = (Element)i.next();
-        if (testSuivant.getName().equals("Row")){
+        if ("Row".equals(testSuivant.getName())){
             
             if(testSuivant.getAttributeValue("Index").equals(String.valueOf(1))){
                 return testSuivant;
@@ -223,7 +132,7 @@ static void insertExperience(Element source){
     
     while(i.hasNext()){
         Element testSuivant = (Element)i.next();
-        if (testSuivant.getName().equals("Row")){
+        if ("Row".equals(testSuivant.getName())){ 
             rowIndexValue = testSuivant.getAttributeValue("Index");
             if(Integer.valueOf(rowIndexValue)>1){
                 if (count){
@@ -243,7 +152,7 @@ static void insertExperience(Element source){
     }
     System.out.println(experienceSQLCode);
     } catch (Exception e){
-        e.printStackTrace();
+        
     }
 }
 
@@ -280,6 +189,9 @@ static String insertEtape(Iterator dataTraveler){
                 temp = data.getTextTrim();
                 temp = makeStringSQLReadable(temp);
                 break;
+            default:
+                break;
+                        
         }
         if("".equals(temp) || temp.isEmpty()){
             notNullValue=false;
@@ -307,7 +219,7 @@ static String insertEtape(Iterator dataTraveler){
                 }
             
             }
-            if(data.getAttributeValue("Type").equals("String")){
+            if("String".equals(data.getAttributeValue("Type"))){
                 temp = "'"+temp+"'";
             }
             requestAttributeValue = requestAttributeValue + temp;
